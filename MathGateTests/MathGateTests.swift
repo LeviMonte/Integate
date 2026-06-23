62bf2f55aa54d192e5e9237fcc229a622f759791
LeviMonte <levimonte18@gmail.com>
2026-06-23 11:39:19 -0400

    Initial Commit

---
+++ MathGateTests/MathGateTests.swift
@@ -0,0 +1,19 @@
+//
+//  MathGateTests.swift
+//  MathGateTests
+//
+//  Created by Levi Monte on 6/23/26.
+//
+
+import Testing
+@testable import MathGate
+
+struct MathGateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ MathGateUITests/MathGateUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  MathGateUITestsLaunchTests.swift
+//  MathGateUITests
+//
+//  Created by Levi Monte on 6/23/26.
+//
+
+import XCTest
+
+final class MathGateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
